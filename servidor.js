75a0411f128a6fd8a54602b50f5a9bf313918866
kamilenna <karla.milenna1@gmail.com>
2024-02-08 16:16:37 -0300

    Finalizando o projeto Chat

--- servidor.js
+++ servidor.js
@@ -3,6 +3,14 @@ const express = require('express');
 const aplicacao = express();
 
 const servidorHttp = http.createServer(aplicacao);
+const io = require('socket.io')(servidorHttp)
+
+io.addListener('connection', (socket) => {
+    console.log("Um usuário conectou")
+    socket.addListener("Nova Mensagem", (msg) => {
+        io.emit("Nova Mensagem", msg)
+    })
+})
 
 aplicacao.use(express.static('public'))
 
